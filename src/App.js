4e4ca1aabffe1a35db4ab401aab1150d8a53bd45
Kenwren Apilado <155591015+Kenwren@users.noreply.github.com>
2024-05-10 15:38:15 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+
+import './App.css';
+import StarRating from './components/StarRating';
+import Modal from './components/Modal';
+
+function App() {
+  return (
+
+    
+      <main className='App container'>
+        <Modal></Modal>
+        <StarRating></StarRating>
+      </main>
+  );
+};
+
+export default App;
